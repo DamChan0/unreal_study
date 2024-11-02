d281309bfb6adc97e618abf88f3122559dfe5a53
damchan0 <damchan113@gmail.com>
2024-11-02 16:11:22 +0900

    change dir

--- README.md
+++
@@ -1,3 +0,0 @@
-# MyProject_study
-
-Developed with Unreal Engine 5
--- Source/MyProject_study/MyProject_study.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MyProject_study.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyProject_study, "MyProject_study" );
